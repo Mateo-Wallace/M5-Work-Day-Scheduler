fd6f2f3dbfbfefb0426ef037bd59bb2ea7bc0e5b
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-29 17:46:19 -0500

    pushing to local but not the right value

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,38 @@
 var weekDay = moment().format("ddd MMM Do, YYYY");
 $("#currentDay").text(weekDay);
 
-var nineInput = document.getElementById('9amInput')
-var nineBtn = document.getElementById('9amBtn')
+// var nineInput = document.getElementById('9amInput')
+// var nineBtn = document.getElementById('9amBtn')
 
-function storeNote() {
-	localStorage.setItem("9am", nineInput.value)
+var inputEls = document.getElementsByClassName('input')
+var saveEls = document.getElementsByClassName('save')
+
+function storeNote(input, time) {
+	localStorage.setItem(time, input.value)
 	renderLocal();
 }
 
 // Determines if item in local storage has a value
-function renderLocal() {
-	var nineAm = localStorage.getItem("9am");
-	if (nineAm == null | nineAm == '') {
+function renderLocal(input, time) {
+	var localTime = localStorage.getItem(time);
+	if (localTime == null | localTime == '' | localTime=='undefined') {
 		return
 	} else {
-		nineInput.value = "";
-		nineInput.value = nineAm;
+		input.value = "";
+		input.value = localTime;
 	}
 }
 
+console.log(inputEls)
 
-renderLocal()
+// saveEls.forEach(input => input.addEventListener('click', renderLocal(input, input.dataset.time)));
 
+// saveEls.forEach(input => input.addEventListener('click', storeNote(input.dataset.time)));
 
+// nineBtn.addEventListener('click', storeNote)
 
+for (i=0; i < saveEls.length; i++) {
+renderLocal(inputEls[i], saveEls[i].dataset.time)
 
-nineBtn.addEventListener('click', storeNote)
+saveEls[i].addEventListener('click', storeNote(inputEls[i], saveEls[i].dataset.time))
+}
