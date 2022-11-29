00311fbe30c22bc03a0486b5b4be6b4e6b2c4712
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-29 17:17:26 -0500

    local storage for 9 am functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
 // Adds current date to the header
 var weekDay = moment().format("ddd MMM Do, YYYY");
 $("#currentDay").text(weekDay);
+
+var nineInput = document.getElementById('9amInput')
+var nineBtn = document.getElementById('9amBtn')
+
+function storeNote() {
+	localStorage.setItem("9am", nineInput.value)
+	renderLocal();
+}
+
+// Determines if item in local storage has a value
+function renderLocal() {
+	var nineAm = localStorage.getItem("9am");
+	if (nineAm == null | nineAm == '') {
+		return
+	} else {
+		nineInput.value = "";
+		nineInput.value = nineAm;
+	}
+}
+
+
+renderLocal()
+
+
+
+
+nineBtn.addEventListener('click', storeNote)
