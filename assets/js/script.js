3d8b9315274f056140a2eaa286d649f6391d9e85
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-29 20:15:47 -0500

    cleaned up code and commented

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,6 @@ $("#currentDay").text(weekDay);
 var inputEls = document.querySelectorAll('.input')
 // Selects each save button
 var saveEls = document.querySelectorAll('.save')
-// Selects each row
-// var rowEls = document.querySelectorAll('.target-row')
 
 // saves user input to local storage
 function storeNote(e) {
@@ -17,43 +15,42 @@ function storeNote(e) {
     const time = button.dataset.time;
     const input = button.parentElement.parentElement.childNodes[3].childNodes[0];
 
-    localStorage.setItem(time, input.value)
-}
+    localStorage.setItem(time, input.value);
+};
 
 // Determines if item in local storage has a value
 function renderLocal(input, time) {
     var localTime = localStorage.getItem(time);
     if (localTime == null | localTime == '' | localTime == 'undefined') {
-        return
+        return;
     } else {
         input.value = "";
         input.value = localTime;
-    }
-}
+    };
+};
 
+// changes color based on if time is in past or future
 function colorChange(button) {
-    const time = +button.dataset.time
-    var currentTime = +moment().format('H')
-    const rowDiv = button.parentElement.parentElement
-    console.log(rowDiv)
+    const time = +button.dataset.time;
+    var currentTime = +moment().format('H');
+    const rowDiv = button.parentElement.parentElement;
 
     if (time < currentTime) {
-        rowDiv.setAttribute('class', 'past')
+        rowDiv.setAttribute('class', 'past');
     } else if (time == currentTime) {
-        rowDiv.setAttribute('class', 'present')
+        rowDiv.setAttribute('class', 'present');
     } else {
-        rowDiv.setAttribute('class', 'future')
-    }
-}
-
-// loops through each row, changes color based on if time is in past or future
-saveEls.forEach((button) => colorChange(button))
+        rowDiv.setAttribute('class', 'future');
+    };
+};
 
 // Renders locally stored values for each input
 for (i = 0; i < saveEls.length; i++) {
-    renderLocal(inputEls[i], saveEls[i].dataset.time)
-}
+    renderLocal(inputEls[i], saveEls[i].dataset.time);
+};
 
-// Adds event listener to each save button
-saveEls.forEach((button) => button.addEventListener("click", storeNote));
-
+// Adds event listener to each save button and checks time to change color
+saveEls.forEach((button) => {
+    button.addEventListener("click", storeNote);
+    colorChange(button);
+});
